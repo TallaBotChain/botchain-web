911fd52ee8beb0b0c2aafe98abbd9382359a4490
Matt Conway <matt@conwaysplace.com>
2018-01-08 12:11:35 -0500

    use docker for deploy and local dev

---
+++ Dockerfile
@@ -0,0 +1,38 @@
+FROM node:8
+
+ENV SRV_DIR /srv
+ENV APP_DIR $SRV_DIR/app
+RUN mkdir -p $SRV_DIR $APP_DIR
+
+# set our node environment, either development or production
+# defaults to production, compose overrides this to development on build and run
+ARG NODE_ENV=production
+ENV NODE_ENV $NODE_ENV
+
+RUN apt-get update -qq && \
+    DEBIAN_FRONTEND=noninteractive apt-get install -q -y \
+        netcat \
+        vim \
+        less \
+        curl \
+        python-dev \
+        python-pip \
+        && \
+    rm -rf /var/lib/apt/lists/*
+
+RUN pip install awscli
+
+# react-static build doesn't seem to work with an external node_modules with
+# PATH/NODE_PATH set accordingly, so install in app dir
+WORKDIR $APP_DIR
+COPY package*.json $APP_DIR/
+RUN npm install && npm cache clean --force
+
+# node_modules excluded from copy due to .dockerignore
+COPY . $APP_DIR/
+
+ENV PATH $APP_DIR/node_modules/.bin:$PATH
+
+RUN react-static build
+
+ENTRYPOINT ["/srv/app/docker-entrypoint.sh"]
---
+++ docker-entrypoint.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+
+# fail fast
+set -e
+
+function wait_for_host_port {
+  if [[ $# != 2 ]]; then echo "usage: $FUNCNAME host port"; return 1; fi
+
+  host=$1
+  port=$2
+  echo "Waiting for $host:$port to become available"
+  while ! nc -z $host $port  > /dev/null 2>&1; do echo .; sleep 2; done
+  echo "The service on $host:$port is now available"
+}
+
+function wait_for_services {
+  for svc in $SERVICES; do
+    host=${svc%%:*}
+    port=${svc##*:}
+    wait_for_host_port ${!host} ${!port}
+  done
+}
+
+function app_init {
+  # Run install if needed - useful in dev
+#  if [[ "$APP_ENV" == "development" ]]; then
+#    npm install
+#  fi
+  :
+}
+export -f app_init
+
+action=$1; shift
+
+case $action in
+
+  app)
+    wait_for_services
+    app_init
+
+    npm start
+  ;;
+
+  compile)
+    wait_for_services
+    app_init
+
+    react-static build
+  ;;
+
+  deploy)
+    wait_for_services
+    app_init
+
+    react-static build
+    if [[ -n $UI_BUCKET ]]; then
+      echo "Copying web files to $UI_BUCKET"
+      aws s3 cp --acl bucket-owner-full-control --recursive \
+        /dist/ s3://$WEB_BUCKET/
+    fi
+  ;;
+
+  console)
+    wait_for_services
+    app_init
+
+    truffle console
+  ;;
+
+  test)
+    wait_for_services
+    npm run compile
+    npm test
+  ;;
+
+  bash)
+    exec bash -il
+  ;;
+
+  exec)
+    exec "$@"
+  ;;
+
+  *)
+    echo "Invalid action: $action"
+    exit 1
+  ;;
+
+esac
