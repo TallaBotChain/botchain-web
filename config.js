41b5f753a3295464b5f675a09fec15a096a74f8b
D Salko <dm.salko@gmail.com>
2018-01-11 13:46:43 -0500

    Updated smart contract address

--- config.js
+++ config.js
@@ -2,19 +2,19 @@ const configs = {
   development: {
     title: '[DEV] Botchain ',
     api_endpoint: 'http://localhost:3001',
-    botchain_contract: "0xaf8153cdc92563dc18aee3470e3dc53da0560830",
+    botchain_contract: "0x8b2c764339b269828eb2548ae1a821244bd0e232",
     etherscan_url: "https://kovan.etherscan.io"
   },
   demo: {
     title: '[TESTNET] Botchain ',
     api_endpoint: 'https://botchain-api.botchain.talla.io',
-    botchain_contract: "0xaf8153cdc92563dc18aee3470e3dc53da0560830",
+    botchain_contract: "0x8b2c764339b269828eb2548ae1a821244bd0e232",
     etherscan_url: "https://kovan.etherscan.io"
   },
   production: { // the same with demo right now
     title: 'Botchain',
     api_endpoint: 'https://botchain-api.botchain.talla.io',
-    botchain_contract: "0xaf8153cdc92563dc18aee3470e3dc53da0560830",
+    botchain_contract: "0x8b2c764339b269828eb2548ae1a821244bd0e232",
     etherscan_url: "https://etherscan.io"
   }
 }
