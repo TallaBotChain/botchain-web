983aaac1a744ba58700d64ca157639445d185e02
D Salko <dm.salko@gmail.com>
2018-03-13 17:58:23 -0400

    Sass stylesheets

--- static.config.js
+++ static.config.js
@@ -1,10 +1,14 @@
 import axios from 'axios'
 import config from './config'
+import ExtractTextPlugin from 'extract-text-webpack-plugin'
 
 export default {
   getSiteProps: ({dev}) => config(dev),
   getRoutes: async () => {
     return [
+      { path: '/',
+        component: 'src/containers/Search'
+      },
       {
         path: '/no_metamask',
         component: 'src/containers/NoMetamask',
@@ -15,20 +19,45 @@ export default {
       }
     ]
   },
-  // webpack: (config, { defaultLoaders }) => {
-  //   config.module.rules = [
-  //     {
-  //       oneOf: [
-  //         {
-  //           test: /\.json$/,
-  //           use: [{ loader: 'json-loader' }],
-  //         },
-  //         defaultLoaders.jsLoader,
-  //         defaultLoaders.cssLoader,
-  //         defaultLoaders.fileLoader,
-  //       ],
-  //     },
-  //   ]
-  //   return config
-  // },
+  webpack: (config, { defaultLoaders, stage }) => {
+    config.plugins.push( new ExtractTextPlugin({
+          filename: getPath => {
+            process.env.extractedCSSpath = getPath('styles.[hash:8].css')
+            return process.env.extractedCSSpath
+          },
+          allChunks: true,
+        }));
+    config.module.rules = [
+      {
+        oneOf: [
+          {
+            test: /\.s(a|c)ss$/,
+            use:
+            stage === 'dev'
+            ? [{ loader: 'style-loader' }, { loader: 'css-loader' }, { loader: 'sass-loader' }]
+            : ExtractTextPlugin.extract({
+              use: [
+                {
+                  loader: 'css-loader',
+                  options: {
+                    importLoaders: 1,
+                    minimize: true,
+                    sourceMap: false,
+                  },
+                },
+                {
+                  loader: 'sass-loader',
+                  options: { includePaths: ['src/'] },
+                },
+              ],
+            }),
+          },
+          defaultLoaders.cssLoader,
+          defaultLoaders.jsLoader,
+          defaultLoaders.fileLoader,
+        ],
+      },
+    ]
+    return config
+  }
 }
